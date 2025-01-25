e348e2af4455d2e40742fe4c719f75d48e6ef77a
Estifanos Girma <estifanosgirma4@gmail.com>
2025-01-25 18:01:14 +0300

    log in

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const form = document.getElementById('registration-form');
+    const feedbackDiv = document.getElementById('form-feedback');
+    form.addEventListener('submit',(submit) => {
+        submit.preventDefault();
+        const userName = document.getElementById('username').value.trim();
+        const password = document.getElementById('password').value.trim();
+        const email = document.getElementById('email').value.trim();
+        let isValid = true;
+        let messages = [];
+        if(userName.length<3)
+        {
+            isValid = false;
+            messages.push('invalid username');
+
+        };
+        if(!email.includes('@')||!email.includes('.'))
+        {
+            isValid = false;
+            messages.push('invalid email must include "@" and "."');
+        };
+        if(password.length < 8)
+        {
+            isValid = false;
+            messages.push('invalid password, must be >=8');
+        }
+        feedbackDiv.style.display = 'block';
+        
+        if(isValid)
+        {
+            feedbackDiv.textContent = 'Registration successful!';
+            feedbackDiv.style.color = '#28a745'
+        }
+        else
+        {
+            feedbackDiv.innerHTML = message.join('<br>'); 
+            feedbackDiv.style.color = '#dc3545'; 
+        }
+
+    });
+});
